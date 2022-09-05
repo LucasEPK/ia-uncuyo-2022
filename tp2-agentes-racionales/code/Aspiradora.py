9d0b226ec0372b9694b64fa19fe18907ab128d3b
LucasEPK <83588493+LucasEPK@users.noreply.github.com>
2022-09-05 09:37:13 -0300

    Add files via upload

---
+++ tp2-agentes-racionales/code/Aspiradora.py
@@ -0,0 +1,17 @@
+class Environment:
+    def __init__(self, sizeX, sizeY, init_posxX, init_posX, init_posY, dirt_race):
+        pass
+
+    def accept_action(self, action):
+        bruh
+
+    def is_dirty(self):
+        bruh
+
+    def get_performance(self):
+        bruh
+
+    def print_enviroment(self):
+        bruh
+
+    
